56e589058eb00a769f89b55ef456b5dc898510f9
Helix48 <sethjhammell@gmail.com>
2022-12-18 02:22:50 -0500

    added dark mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,46 @@
 import Home from "./home/home";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import "./App.css";
+import { useState } from "react";
+import { FormControlLabel, Switch } from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const lightTheme = createTheme();
 
 function App() {
+  const [darkMode, setDarkMode] = useState<boolean>(true);
+
+  const themeToggle = (
+    <Switch
+      checked={darkMode}
+      onChange={() => {
+        setDarkMode(!darkMode);
+      }}
+    />
+  );
+
   return (
     <div>
-      <Home />
+      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+        <CssBaseline />
+        <Home />
+        <div className="themeToggle">
+          <FormControlLabel
+            control={themeToggle}
+            label="Dark Mode"
+          ></FormControlLabel>
+        </div>
+      </ThemeProvider>
     </div>
   );
 }
+
+// toggle
 
 export default App;
